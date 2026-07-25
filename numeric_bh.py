7056139606ea1edddb20d8d43b88f9f4e2126fd2
Jose Miguel Sedano <sedanojosemiguel@gmail.com>
2026-07-25 12:13:41 -0500

    Inicio del proyecto Numeric BH

---
+++ numeric_bh.py
@@ -0,0 +1,10 @@
+""" Numeric BH: Conversión de bases numéricas """
+
+# Menú Principal
+
+def menu_principal():
+   """ Función para el menú principal """
+   while True:
+      print("<<< NUMERIC BH >>>")
+      print("Bienvenido al sistema de Numeric BH")
+      print("1. Conversión de bases numéricas")
